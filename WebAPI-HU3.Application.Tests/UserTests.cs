3c701a815fada3a7f01b15db02cc4fcf68d46190
Luisa <lfrm24680@gmail.com>
2025-11-07 14:51:32 -0500

    tests

---
+++ WebAPI-HU3.Application.Tests/UserTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using WebApi_HU3.Domain.Entities;
+
+public class UserTests
+{
+    [Fact]
+    public void Should_Hash_And_Verify_Password()
+    {
+        var hasher = new PasswordHasher<User>();
+        var user = new User { Username = "testuser" };
+        var password = "Contraseña";
+
+        var hashed = hasher.HashPassword(user, password);
+        var result = hasher.VerifyHashedPassword(user, hashed, password);
+        
+        Assert.Equal(PasswordVerificationResult.Success, result);
+    }
+}
